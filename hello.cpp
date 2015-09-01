a2e01a9a61a1c1f211547c0c1ade7351dacf98cf
wrembish <wrembish@udel.edu>
2015-09-01 16:06:46 +0000

    changed hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
 int main(){
+    int x;
+    int y = 97;
     std::cout << "What a world this is!" << std::endl;
+    std::cin >> x;
+    std::cout << x + y << std::endl;
+    
 }
